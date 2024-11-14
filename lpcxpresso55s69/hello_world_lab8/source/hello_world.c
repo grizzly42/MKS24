3c418ffe940828c31127931911b6d4ab378cca0c
grizzly42 <240671@vutbr.cz>
2024-11-14 09:37:21 +0100

    nxp

---
+++ lpcxpresso55s69/hello_world_lab8/source/hello_world.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017, 2024 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "peripherals.h"
+
+#include "fsl_power.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+static uint8_t state = 1;
+void TimerRQ(uint32_t flags){
+    PRINTF("hello from irq.\r\n");
+
+}
+void timer2IRQ(uint32_t flags){
+	PRINTF("PWM\r\n");
+
+
+}
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+
+	 char ch;
+    /* Init board hardware. */
+    /* set BOD VBAT level to 1.65V */
+    POWER_SetBodVbatLevel(kPOWER_BodVbatLevel1650mv, kPOWER_BodHystLevel50mv, false);
+    /* attach main clock divide to FLEXCOMM0 (debug console) */
+    CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
+
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitDebugConsole();
+    BOARD_InitPeripherals();
+
+#if !defined(DONT_ENABLE_FLASH_PREFETCH)
+    /* enable flash prefetch for better performance */
+    SYSCON->FMCCR |= SYSCON_FMCCR_PREFEN_MASK;
+#endif
+
+    PRINTF("hello world.\r\n");
+    CTIMER_StartTimer(CTIMER1_PERIPHERAL);
+    CTIMER_StartTimer(CTIMER2_PERIPHERAL);
+
+    while (1)
+    {
+        ch = GETCHAR();
+        PUTCHAR(ch);
+    }
+}
