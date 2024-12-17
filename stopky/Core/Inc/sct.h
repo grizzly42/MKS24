8e0344f127fbea20cb8487f87fa8de321f981229
grizzly42 <240671@vutbr.cz>
2024-12-17 18:05:59 +0100

    xdd

---
+++ stopky/Core/Inc/sct.h
@@ -0,0 +1,20 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 10, 2024
+ *      Author: 240671
+ */
+
+#ifndef SRC_SCT_H_
+#define SRC_SCT_H_
+#include "main.h"
+
+void sct_init(void);
+
+void sct_led(uint32_t value);
+
+void sct_value(uint16_t value, uint8_t led, uint8_t DecPoint);
+void sct_bargraph_only(uint16_t lednum);
+void sct_uni_bar(uint16_t ledBinI);
+
+#endif /* SRC_SCT_H_ */
